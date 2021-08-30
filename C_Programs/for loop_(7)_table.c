82c9524019520191f7abe9c2bea11b5f9212c073
SaurabhKathawate <saurabhkathawate5@gmail.com>
2021-08-30 11:53:50 +0530

    19aug

---
+++ C_Programs/for loop_(7)_table.c
@@ -0,0 +1,14 @@
+//7-wap to accept a no. and print table of that no. using for loop.
+#include<stdio.h>
+int main()
+{
+	int num,table,i;
+	printf("Enter number: ");
+	scanf("%d",&num);
+	for(i=1;i<=10;i++)
+	{
+		table=num * i;
+		printf("\n%d * %d = %d",num,i,table);
+	}
+}
+
