6cbcaf155133f064cc6d342e4e685d847cf3ec82
SaurabhKathawate <89062132+SaurabhKathawate@users.noreply.github.com>
2021-09-05 15:39:37 +0530

    Revert "Develop"

--- C_Programs/basic_(2)_num variable.c
+++
@@ -1,8 +0,0 @@
-//2-wap to declare a integer variable num and store value 5 in it and then print the value stored in num variable.
-#include<stdio.h>
-int main()
-{
-	int num=5;
-	printf("num=%d",num);
-}
-
