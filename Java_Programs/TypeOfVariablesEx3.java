0073066d9b413aa8e2b89a4b0ddca770d62878d7
SaurabhKathawate <saurabhkathawate5@gmail.com>
2021-09-11 10:18:52 +0530

    09Sept

---
+++ Java_Programs/TypeOfVariablesEx3.java
@@ -0,0 +1,12 @@
+//Java Variable Example: Widening
+
+    public class TypeOfVariablesEx3
+    {
+    public static void main(String arg[])
+    {
+    int a=10;
+    float f=a;
+    System.out.println(a);
+    System.out.println(f);
+    }
+    }
